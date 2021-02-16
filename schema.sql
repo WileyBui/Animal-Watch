7290fc6f030dcf0e3da75c343cd39543984b560c
Jessie Speert <speer034@umn.edu>
2021-02-16 15:40:52 -0800

    Pushing schema.sql; the file has not been run/used to create the database yet, but serves as a rough draft for what will be created.

---
+++ schema.sql
@@ -0,0 +1,39 @@
+drop table Users;
+drop table Tags;
+drop table Animals;
+drop table Posts;
+drop table Comments;
+
+create table Users (
+	id SERIAL PRIMARY KEY,
+	name varchar(255)
+);
+
+create table Tags (
+	id SERIAL PRIMARY KEY,
+	tag varchar(255)
+);
+
+create table Animals (
+	id SERIAL PRIMARY KEY,
+	name varchar(255),
+	endangerment_level int,
+	category int,
+	tag_id int references Tags
+);
+
+create table Posts (
+	id SERIAL PRIMARY KEY,
+	user_id int references Users,
+	animal_id int references Aniimals,
+	text varchar(255),
+	timestamp time
+);
+
+create table Comments (
+	id SERIAL PRIMARY KEY,
+	user_id int references Users,
+	comment_id int references Comments,
+	text varchar(255),
+	timestamp time
+);
