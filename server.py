a5dc32641059a6117285c77260d134c1f0e1f7e6
Wiley Bui <17buih@gmail.com>
2021-02-24 20:33:14 -0600

    Pushing Wiley branch to pull main branch

--- server.py
+++ server.py
@@ -20,10 +20,35 @@ def page_login():
 def page_add_animal():
     return render_template("addAnimal.html")
 
-@app.route('/feed')
-def page_feed():
-    return render_template("feed.html")
 
+def get_username_by_id(id):
+    with db.get_db_cursor(False) as cur:
+        cur.execute("SELECT * FROM Users WHERE id = ", id)
+
+@app.route('/feed', methods=['GET'])
+def page_feed():
+    with db.get_db_cursor(False) as cur:
+        cur.execute("""
+                    SELECT
+                        Posts.id,
+                        Users.name,
+                        Posts.text,
+                        Animals.imageURL,
+                        Tags.tag,
+                        Tags.tag_bootstrap_color
+                    FROM Posts
+                    JOIN Users
+                        ON Posts.user_id = Users.id
+                    JOIN Animals
+                        ON Posts.animal_id = Animals.id
+                    JOIN Tags
+                        ON Animals.tag_id = Tags.id
+                    ORDER BY Posts.timestamp DESC;
+                    """)
+        # cur = [record for record in cur];
+        
+        return render_template("feed.html", dataList=cur)
+    
 @app.route('/lookup')
 def page_lookup():
     return render_template("animal_look_up.html")
