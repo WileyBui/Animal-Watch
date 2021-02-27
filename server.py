187123fdff3daa2175994af02cacb3c3528e7fba
Ray Lauffer <lauff007@umn.edu>
2021-02-27 13:47:12 -0600

    trying to pull from Jessie

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, request, g, redirect, url_for, jsonify
-
+from flask import Flask, render_template, request, g, redirect, url_for, jsonify, abort, session
+from urllib.parse import urlencode
+import os
 import db
+from auth0 import auth0_setup, require_auth, auth0
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = os.environ["FLASK_SECRET_KEY"]
+
+# have the DB submodule set itself up before we get started. groovy.
+@app.before_first_request
+def initialize():
+    db.setup()
+    auth0_setup()
 
 @app.route('/')
 def page_landing():
@@ -12,13 +22,71 @@ def page_landing():
 def page_signup():
     return render_template("signup.html")
 
+### AUTH0:
 @app.route('/login')
 def page_login():
-    return render_template("MemberLoginPage.html")
+    if 'profile' in session:
+        return redirect(url_for('test_auth'))
+    else:
+        return auth0().authorize_redirect(redirect_uri=url_for('callback', _external=True))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    params = { 'returnTo': url_for('home', _external=True), 'client_id': os.environ['AUTH0_CLIENT_ID'] }
+    return redirect(auth0().api_base_url + '/v2/logout?' + urlencode(params))
+
+@app.route('/callback')
+def callback():
+    auth0().authorize_access_token()
+    resp = auth0().get('userinfo')
+    userinfo = resp.json()
+
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+
+    with db.get_db_cursor(commit=True) as cur:
+        users_id = session['profile']['user_id']
+        users_name = session['profile']['name']
+        cur.execute("insert into Users (id, users_name) values (%s. %s)", (users_id, users_name))
+    return redirect('/test_auth')
+
+@app.route('/test_auth')
+@require_auth
+def test_auth():
+    return render_template("main.html", profile=session['profile'])
+
+
 
 @app.route('/add')
+@require_auth
 def page_add_animal():
     return render_template("addAnimal.html")
+
+
+@app.route('/add', methods=['POST'])
+def processAddAnimal():
+    with db.get_db_cursor(commit=True) as cur:
+        users_id = session['profile']['user_id']
+        animal_id = request.form.get("species")
+        post_text = request.form.get("classification")
+        post_location = request.form.get("range")
+        #latitude =
+        #longitude =
+        # Get image from the form
+        #with open(request.form.get("image"), "rb") as image:
+            # Encode image data into a string
+         #   post_image = base64.b64encode(image.read())
+          #  console.logger.info(post_image)
+        post_image = request.form.get("image")
+        post_time = str(datetime.now())
+        cur.execute("insert into Posts (users_id, animal_id, post_text, imageURL, post_time) values (%s, %s, %s, %s, %s);", (users_id, animal_id, post_text, post_image, post_time))
+        #cur.execute("insert into Locations (user_id, animal_id, lat, long) values (%s, %s, %s, %s);" (user_id, animal_id, lat, long))
+        return redirect(url_for("/feed"))
 
 @app.route('/feed', methods=['GET'])
 def page_feed():
@@ -58,12 +126,6 @@ def page_feed():
 def page_lookup(id):
     return render_template("post_lookup.html")
 
-
-# have the DB submodule set itself up before we get started. groovy.
-@app.before_first_request
-def initialize():
-    db.setup()
-
 @app.route('/home')
 def home():
     user_name = request.args.get("userName", "unknown")
@@ -97,5 +159,3 @@ def api_foo():
         }
     }
     return jsonify(data)
-
-#hi
